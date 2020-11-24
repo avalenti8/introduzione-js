77b99100b0610efb043de6eeea2880624c95b2d4
Alessandro <38206235+avalenti8@users.noreply.github.com>
2020-11-24 19:06:00 +0100

    Create function.js

---
+++ function.js
@@ -0,0 +1,19 @@
+'use strict';
+
+let name = prompt("Con chi sto parlando");
+
+alert("Ciao " + name);
+
+alert("Ora ti farò una domanda di cruciale importanza");
+alert("Se rispondi di no vieni immediatamente bruciato al rogo");
+let string = "vai " + "uomo";
+alert("Se dici si sei una persona saggia, " + string);
+let hobby = prompt("Ti piace il calcio?");
+
+let conf = confirm("Se hai mai giocato a calcio clicca Ok, altrimenti Annulla");
+
+let goals = prompt("Nel caso tu avessi giocato, quanti goal hai fatto in carriera?");
+
+alert("mm, solo " + goals + " goal...");
+
+alert("Pensa che io ne ho fatti all'incirca " + "5" + "0");
